954ebb7d84783382955326a79fdb5bc60c2f8fe8
kylayouu <quilos.kyla@gmail.com>
2021-06-01 21:04:00 -0700

    Deleted previous script.js content. Revised script.js with pokemonList variable with an array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert('Hello World');
-
-favoriteFood = "Xi'an Noodles";
-document.write(favoriteFood);
+pokemonList = [
+    {name: 'Bulbasaur', height: 7, type: ['grass', 'poison']},
+    {name: 'Charmander', height: 6, type: 'fire'},
+    {name: 'Pikachu', height: 4, type: 'electric'}
+];
+    
