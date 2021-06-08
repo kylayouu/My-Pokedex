56de2395a99f139ffe423289b9ba19bd72ea9710
Kyla Quilos <quilos.kyla@gmail.com>
2021-06-08 16:09:12 -0700

    Added loop for list of Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 pokemonList = [
-    {name: 'Bulbasaur', height: 7, type: ['grass', 'poison']},
-    {name: 'Charmander', height: 6, type: 'fire'},
-    {name: 'Pikachu', height: 4, type: 'electric'}
+    {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
+    {name: 'Charmander', height: 0.6, type: 'fire'},
+    {name: 'Pikachu', height: 0.4, type: 'electric'}
 ];
-    
+
+// loop to list out each Pokemon's information
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name + ' (height:' + pokemonList[i].height + ')' + '<br/>');
+}
