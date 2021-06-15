0848207717f128b0cf427fd6272d2eb5d8484484
Kyla Quilos <quilos.kyla@gmail.com>
2021-06-15 15:34:04 -0700

    Created IIFE to return functions add() and getAll(), updated forEach() loop accordingly

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,26 @@
-pokemonList = [
-    {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
-    {name: 'Charmander', height: 0.6, type: 'fire'},
-    {name: 'Pikachu', height: 0.4, type: 'electric'}
-];
+// IIFE to return functions add() and getAll()
+let pokemonRepository = (function() {
+    let pokemonList = [
+        {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
+        {name: 'Charmander', height: 0.6, type: 'fire'},
+        {name: 'Pikachu', height: 0.4, type: 'electric'}
+    ];
+
+    function getAll() {
+        return pokemonList;
+    };
+
+    function add(item) {
+        return pokemonList.push(item);
+    };
+
+    return {
+        getAll: getAll,
+        add: add,
+    };
+})();
 
 // forEach() loop to list out each Pokemon's information
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
     document.write(pokemon.name + ' (height: ' + pokemon.height + '), (type: ' + pokemon.type + ')' + '<br>');
 });
