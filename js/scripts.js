c4243da2eaa55c952bad860f366c1d88fe7b0d45
Kyla Quilos <quilos.kyla@gmail.com>
2021-08-23 15:57:48 -0700

    Added modal container and used fetch to pull the pokemon list from API

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,57 @@
 // IIFE to return functions add(), getAll(), addListItem()
 let pokemonRepository = (function() {
-  let pokemonList = [
-    {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
-    {name: 'Charmander', height: 0.6, type: 'fire'},
-    {name: 'Pikachu', height: 0.4, type: 'electric'}
-  ];
+  // let pokemonList = [
+  //   {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
+  //   {name: 'Charmander', height: 0.6, type: 'fire'},
+  //   {name: 'Pikachu', height: 0.4, type: 'electric'}
+  // ];
+
+
+  const apiURL = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  let pokemonList = [];
+
+  loadList = () => {
+
+  fetch(apiURL)
+  .then(response=>response.json())
+  .then(response =>
+  {
+    pokemonList = response.results;
+    pokemonList.forEach(pokemon=>{
+    let newPokemonList = document.querySelector(".pokemon-list");
+      let listItemPokemon = document.createElement("li");
+      let button = document.createElement("button");
+      button.innerText = pokemon.name;
+      button.classList.add("pokemon-name-btn");
+      button.addEventListener('click', function() {
+
+        fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+        .then(response=>response.json())
+        .then(response=>{
+          console.log(response)
+          console.log(response.sprites.back_shiny)
+
+           let pokemonDetails = `Height: ${response.height}`
+       
+
+        modal.showModal(`${pokemon.name}`, `${pokemonDetails}`, `${response.sprites.back_shiny}`);
+
+
+        })
+        .catch(err=>console.error(err))
+
+       
+        // alert(pokemonDetails);
+      });
+      listItemPokemon.appendChild(button);
+      newPokemonList.appendChild(listItemPokemon);
+      })
+      })
+      .catch(error=>console.log(error))
+  }
 
   function getAll() {
-    return pokemonList;
+    loadList();
   };
 
   function add(pokemon) {
@@ -18,32 +62,13 @@ let pokemonRepository = (function() {
     console.log(pokemon)
   }
 
-  function addListItem(pokemon) {
-    pokemonList = document.querySelector('.pokemon-list');
-
-    listItem = document.createElement('li');
-    button = document.createElement('button');
-    button.innerText = pokemon.name;
-    button.classList.add('pokemon-name-btn');
-
-    listItem.appendChild(button);
-    pokemonList.appendChild(listItem);
-
-    // When button is clicked, the pokemon's data is displayed on the console.
-    button.addEventListener('click', function() {
-      showDetails(pokemon)
-    })
-  };
-
   return {
     getAll: getAll,
     add: add,
     showDetails: showDetails,
-    addListItem: addListItem
+    loadList: loadList,
   };
 })();
 
 // forEach() loop to display the buttons for the Pokemon list
-pokemonRepository.getAll().forEach(function(pokemon) {
-  pokemonRepository.addListItem(pokemon);
-});
+pokemonRepository.getAll();
